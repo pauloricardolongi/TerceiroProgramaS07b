d50fcbe254baacb552e2907fe7739dbcdd27efbd
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-03 05:06:32 -0300

    continuação triangulo, delegação de classe

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,34 +10,34 @@ public class Programa {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		Triangulo x, y;
 		x = new Triangulo();
-		y= new  Triangulo();
+		y = new Triangulo();
 		System.out.println("Entre com as  medidas do triangulo X");
 		x.a = sc.nextDouble();
-		x.b =sc.nextDouble();
-		x.c= sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		System.out.println("Entre com as medidas do triangulo Y");
 		y.a = sc.nextDouble();
-		y.b =sc.nextDouble();
-		y.c  = sc.nextDouble();
-		double p= (x.a + x.b + x.c)/2.0;
-		double areaX = Math.sqrt(p * (p - x.a)* (p - x.b)* (p-x.c));
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-	     p= (y.a + y.b + y.c)/2.0;
-		double areaY = Math.sqrt(p * (p - y.a)* (p - y.b)* (p-y.c));
 		
+		double areaX = x.area();
+
+		
+		double areaY = y.area();
+
 		System.out.printf("Triangul X area %.4f%n", areaX);
 		System.out.printf("Triangul Y area %.4f%n", areaY);
-		
+
 		if (areaX > areaY) {
 			System.out.println("Maior area: X");
-		}
-		else {
+		} else {
 			System.out.println("Maior area: Y");
 		}
-		
+
 		sc.close();
 
 	}
--- src/entities/Triangulo.java
+++ src/entities/Triangulo.java
@@ -6,6 +6,12 @@ public class Triangulo {
 		public double a;
 		public double b;
 		public double c;
+		
+		public double area() {
+			double p = (a + b + c)/2.0;
+			double resultado = Math.sqrt(p * (p - a)* (p - b)* (p - c));
+			return resultado;
+		}
 
 	}
 
